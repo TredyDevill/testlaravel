24f6263948d1b52693a9961795c2fdf4f5e284e4
TredyDevill <kadekpandu@gmail.com>
2017-07-27 14:22:20 +0700

    percobaan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                  PERCOBAAN PERTAMA
+                  Percobaan Tegar Sukses!!
                 </div>
             </div>
         </div>
