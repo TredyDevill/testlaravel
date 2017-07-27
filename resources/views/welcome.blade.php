03a996ea9edde092f01da8e24ab8cf3c1b537d36
TredyDevill <kadekpandu@gmail.com>
2017-07-27 10:55:38 +0700

    commit keenam

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Pandu Jelek
+                    Pandu Ganteng.
                 </div>
             </div>
         </div>
