63a9bf9dedd70bba6eafcb4a803ff15884edbeb9
nadiahaulia <nadiahnurul.na@gmail.com>
2017-07-27 10:50:37 +0700

    yang kelima

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Dude!
+                    Pandu Jelek
                 </div>
             </div>
         </div>
