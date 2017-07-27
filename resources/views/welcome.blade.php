cf29df04769dd26bbf5fcd80b0a1f1a8e2e68c83
nadiahaulia <nadiahnurul.na@gmail.com>
2017-07-27 10:10:08 +0700

    third commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome To Laravel!
+                    Laravel!
                 </div>
             </div>
         </div>
