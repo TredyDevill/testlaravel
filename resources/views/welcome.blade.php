3997b2e8df9ccb7eff4b67c2adc3bf6f7c4f8b2d
nadiahaulia <nadiahnurul.na@gmail.com>
2017-07-27 10:56:00 +0700

    ke enam

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Pandu Jelek
+                    Kadek Pandu
                 </div>
             </div>
         </div>
